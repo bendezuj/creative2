9692a9b76a8e9c49ed5db617cfd4c8ab579c24a8
Jeanpierre Bendezu <jeanpierrebendezu@gmail.com>
2020-10-17 22:10:04 -0600

    Semifinal commit

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("It is working")
+// 619 jokes
+// Jokes 7 through 11 don't exist. There may be more.
+let name = "";
+document.getElementById("nameSubmit").addEventListener("click", function(event) {
+      event.preventDefault();
+      const val = document.getElementById("nameInput").value;
+      if (val === "") {
+        name = "";
+      }
+      else{
+        name = val;
+      }
+  document.getElementById("nameShow").innerHTML = name;
+});
+
+document.getElementById("jokesSubmit").addEventListener("click", function(event) {
+      event.preventDefault();
+      const value = document.getElementById("jokeInput").value;
+      if (value === "") {
+        value = "random";
+      } else if (value > 6 && value < 12) {
+        while (value > 619) {
+          value--;
+        }
+      }
+  console.log(value);
+  let url = ""
+  if(name != ""){
+    url = "http://api.icndb.com/jokes/" + value + "?firstName=" + name;
+  }
+  else{
+    url = "http://api.icndb.com/jokes/" + value;
+  }
+  debugger
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      let results = "";
+      results += "<p>"
+      results += "<p>Here :)  ->" + json.value.joke + "</p>"
+      results += "</p>";
+      document.getElementById("jokeResults").innerHTML = results;
+    });
+});
